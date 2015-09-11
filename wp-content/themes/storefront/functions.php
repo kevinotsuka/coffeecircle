ea4121c269b28faec040cbff12296d9ae9814ede
kevinotsuka <otsukasan@gmail.com>
2015-09-11 16:43:22 +0900

    updating functions.php to remove billing

--- wp-content/themes/storefront/functions.php
+++ wp-content/themes/storefront/functions.php
@@ -14,3 +14,25 @@ require get_template_directory() . '/inc/init.php';
  * Note: Do not add any custom code here. Please use a child theme so that your customizations aren't lost during updates.
  * http://codex.wordpress.org/Child_Themes
  */
+add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
+ 
+function custom_override_checkout_fields( $fields ) {
+    unset($fields['billing']['billing_first_name']);
+    unset($fields['billing']['billing_last_name']);
+    unset($fields['billing']['billing_company']);
+    unset($fields['billing']['billing_address_1']);
+    unset($fields['billing']['billing_address_2']);
+    unset($fields['billing']['billing_city']);
+    unset($fields['billing']['billing_postcode']);
+    unset($fields['billing']['billing_country']);
+    unset($fields['billing']['billing_state']);
+    unset($fields['billing']['billing_phone']);
+    unset($fields['order']['order_comments']);
+    unset($fields['billing']['billing_address_2']);
+    unset($fields['billing']['billing_postcode']);
+    unset($fields['billing']['billing_company']);
+    unset($fields['billing']['billing_last_name']);
+    unset($fields['billing']['billing_email']);
+    unset($fields['billing']['billing_city']);
+    return $fields;
+}
