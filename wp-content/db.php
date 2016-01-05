45b06c58629496357c994de26dac68a831899b23
vyoungnyc@gmail.com <vyoungnyc@gmail.com>
2016-01-05 23:07:27 +0900

    done

--- wp-content/db.php
+++ wp-content/db.php
@@ -44,7 +44,8 @@ define( 'PG4WP_LOG_ERRORS', false);
 define( 'PG4WP_INSECURE', true);
 
 // Not entirely sure why this needs to be set, but it seems to be required since wp version 4
-define('WP_USE_EXT_MYSQL', true);
+// SET THIS TO TRUE IF USING POSTGRES P4WP
+define('WP_USE_EXT_MYSQL', false);
 
 // This defines the directory where PG4WP files are loaded from
 //   2 places checked : wp-content and wp-content/plugins
