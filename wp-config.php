0330edc2abf3a2b5438d31e36a5258dbc01ec7a1
vyoungnyc@gmail.com <vyoungnyc@gmail.com>
2016-01-07 09:40:33 +0900

    fix site_url

--- wp-config.php
+++ wp-config.php
@@ -38,7 +38,7 @@ define('DB_COLLATE', '');
 
 define('MYSQL_CLIENT_FLAGS', MYSQLI_CLIENT_SSL);  
 
-define( 'WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/path/to/wordpress' );
+define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME']);
 
 /**#@+
  * Authentication Unique Keys and Salts.
