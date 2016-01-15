6d3219281caac411fdbeb350b710429579c16fb2
kevinotsuka <otsukasan@gmail.com>
2016-01-15 19:07:42 +0900

    testing adding eng echo all cats to end of cat menu

--- wp-content/themes/savoy-child/includes/woocommerce/woocommerce.php
+++ wp-content/themes/savoy-child/includes/woocommerce/woocommerce.php
@@ -227,7 +227,6 @@
 	}
 	
 	
-	
 	/*
 	 *	Product category menu
 	 */
@@ -331,6 +330,10 @@
 		$output = $output . $sub_output;
 		
 		echo $output;
+
+		/*trying to add all filter at end of menu*/
+		echo '<li' . $all_categories_class . '><a href="' . esc_url ( $page_url ) . '">' . esc_html__( 'All', 'nm-framework' ) . '</a></li>';
+
 	}
 	
 	
