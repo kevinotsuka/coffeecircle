a3f7440c8b5ef87254e1ae2bf32ef11f69298c94
kevinotsuka <otsukasan@gmail.com>
2016-03-31 10:48:19 +0900

    adding kilo changes

--- wp-content/plugins/woocommerce/templates/single-product/price.php
+++ wp-content/plugins/woocommerce/templates/single-product/price.php
@@ -16,7 +16,7 @@ global $product;
 ?>
 <div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
 
-	<p class="price"><?php echo $product->get_price_html(); ?> <div> <?php echo ' /kg' ?> </div> </p>
+	<p class="price"><?php echo $product->get_price_html(); ?> <div> <?php echo ' /単位' ?> </div> </p>
 
 	<meta itemprop="price" content="<?php echo esc_attr( $product->get_price() ); ?>" />
 	<meta itemprop="priceCurrency" content="<?php echo esc_attr( get_woocommerce_currency() ); ?>" />
--- wp-content/themes/savoy/woocommerce/global/quantity-input.php
+++ wp-content/themes/savoy/woocommerce/global/quantity-input.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 <div class="nm-quantity-wrap">
-	<label><?php esc_html_e( 'キロ数', 'nm-framework' ); ?></label>
+	<label><?php esc_html_e( '単位数量', 'nm-framework' ); ?></label>
     <div class="quantity">
         <input type="number" step="<?php echo esc_attr( $step ); ?>" <?php if ( is_numeric( $min_value ) ) : ?>min="<?php echo esc_attr( $min_value ); ?>"<?php endif; ?> <?php if ( is_numeric( $max_value ) ) : ?>max="<?php echo esc_attr( $max_value ); ?>"<?php endif; ?> name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $input_value ); ?>" title="<?php _ex( 'Qty', 'Product quantity input tooltip', 'woocommerce' ) ?>" class="input-text qty text" size="4" />
     </div>
