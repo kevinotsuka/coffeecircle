959884043b0ccb519b162f72a8166ff9d3c2e71e
vyoungnyc@gmail.com <vyoungnyc@gmail.com>
2016-04-01 18:13:52 +0900

    Update woo 2.5.5

--- wp-content/plugins/woocommerce/templates/single-product/price.php
+++ wp-content/plugins/woocommerce/templates/single-product/price.php
@@ -2,6 +2,14 @@
 /**
  * Single Product Price, including microdata for SEO
  *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/price.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you (the theme developer).
+ * will need to copy the new files to your theme to maintain compatibility. We try to do this.
+ * as little as possible, but it does happen. When this occurs the version of the template file will.
+ * be bumped and the readme will list any important changes.
+ *
+ * @see     http://docs.woothemes.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
  * @version 2.4.9
@@ -16,7 +24,7 @@ global $product;
 ?>
 <div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
 
-	<p class="price"><?php echo $product->get_price_html(); ?> <div> <?php echo ' /単位' ?> </div> </p>
+	<p class="price"><?php echo $product->get_price_html(); ?></p>
 
 	<meta itemprop="price" content="<?php echo esc_attr( $product->get_price() ); ?>" />
 	<meta itemprop="priceCurrency" content="<?php echo esc_attr( get_woocommerce_currency() ); ?>" />
