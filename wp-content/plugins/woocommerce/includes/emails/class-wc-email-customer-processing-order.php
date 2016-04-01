b8334155d0ff95797c6fade2694fdb93baecc09c
vyoungnyc@gmail.com <vyoungnyc@gmail.com>
2016-04-01 19:01:14 +0900

    04

--- wp-content/plugins/woocommerce/includes/emails/class-wc-email-customer-processing-order.php
+++ wp-content/plugins/woocommerce/includes/emails/class-wc-email-customer-processing-order.php
@@ -1,6 +1,6 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
@@ -58,60 +58,77 @@ class WC_Email_Customer_Processing_Order extends WC_Email {
 
 			$this->replace['order-date']   = date_i18n( wc_date_format(), strtotime( $this->object->order_date ) );
 			$this->replace['order-number'] = $this->object->get_order_number();
-
+                        $customer_id = $this->object->post->post_author;
+                        $customer_code = get_the_author_meta('customercode', $customer_id);
 
 
   /** Send order details to notification server. **/
-  $order_list = array();
-  foreach ($this->object->get_items() as $val) {
-    $vendor_name = $val['categories'];
-    if (!array_key_exists($vendor_name, $order_list)) {
-      $order_list[$vendor_name] = array('roaster' => $vendor_name, 'orders'=>array());
-    }
+                 $order_list = array();
+                 foreach ($this->object->get_items() as $val) {
+                     $vendor_name = $val['roaster'];
+                     $vendor_email = $val['roaster_email'];
+                     if (!array_key_exists($vendor_name, $order_list)) {
+                         $order_list[$vendor_name] = array(
+                             'roaster' => $vendor_name,
+                             'roaster_email' => $vendor_email,
+                             'orders' => array()
+                         );
+                     }
+                     $product_id = $val['product_id'];
+                     $order_meta = get_post_custom($order_id);
+                     $product_meta = get_post_custom($product_id);
+                     
+                     $orders = array(
+                         'product_name' => $val['name'],
+                         'product_code' => $product_meta['product_code'][0],
+                         'unit_size' => $product_meta['unit_size'][0],
+                         'unit_quantity' => $product_meta['unit_quantity'][0],
+                         'currency' => $order_meta['_order_currency'][0],
+                         'quantity' => $val['qty'],
+                         'cost' => '&yen;' . $val['line_subtotal']
+                     );
+                     array_push($order_list[$vendor_name]['orders'], $orders);
+                 }
 
-    $orders = array(
-      'product_name' => $val['name'],
-      'quantity'=> $val['qty'],
-      'cost'=> '&yen;' . $val['line_subtotal'],
-    );
-    array_push($order_list[$vendor_name]['orders'], $orders);
-  }
-
-  $order_data = array(
-    'order_id' => $order_id,
-    'order_date' => date_i18n(wc_date_format(), strtotime($this->object->order_date)),
-    'notes' => $this->object->customer_note,
-    'customer_email' => $this->object->billing_email,
-    'customer_telephone' => $this->object->billing_phone,
-    'shipping_address' => $this->object->get_address(),
-    'orders' =>  $order_list,
-    'totals' => $this->object->get_order_item_totals()
-  );
+                 $order_data = array(
+                 	'customer_code' => $customer_code,
+                     'order_id' => $order_id,
+                     'order_date' => date_i18n(wc_date_format(), strtotime($this->object->order_date)),
+                     'notes' => $this->object->customer_note,
+                     'customer_email' => $this->object->billing_email,
+                     'customer_telephone' => $this->object->billing_phone,
+                     'shipping_address' => $this->object->get_address(),
+                     'orders' => $order_list,
+                     'totals' => $this->object->get_order_item_totals()
+                 );
 
   if (array_key_exists($order_data['shipping_address']['state'], $this->states)) {
     $order_data['shipping_address']['state'] = $this->states[$order_data['shipping_address']['state']];
   }
 
-  $curl_url = "http://services.coffeeconnection.jp/newOrder";
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $curl_url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_HEADER, false);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
-  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-  curl_setopt($ch, CURLOPT_TIMEOUT, 20);
-  curl_setopt($ch, CURLOPT_POST, true);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($order_data));
-  $response = curl_exec($ch);
-  $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-  if ( $status != 204 ) {
-    error_log("Error: call to URL $curl_url failed with status $status, response $response, curl_error " . curl_error($ch) . ", curl_errno " . curl_errno($ch));
-  } else {
-    error_log("Successfully sent order_data: " . json_encode($order_data) . " \nresponse: $response , status: $status");
-  }
-  curl_close($ch);
-
+                 $curl_url = "http://services.coffeeconnection.jp/newOrder2";
+                 $ch       = curl_init();
+                 curl_setopt($ch, CURLOPT_URL, $curl_url);
+                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                 curl_setopt($ch, CURLOPT_HEADER, false);
+                 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                     "Content-type: application/json"
+                 ));
+                 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+                 curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+                 curl_setopt($ch, CURLOPT_POST, true);
+                 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($order_data));
+                 error_log("orders: ". json_encode($order_data));
+                 
+                 $response = curl_exec($ch);
+                 $status   = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+                 
+                 if ($status != 204) {
+                     //error_log("Error: call to URL $curl_url failed with status $status, response $response, curl_error " . curl_error($ch) . ", curl_errno " . curl_errno($ch));
+                 } else {
+                     //error_log("Successfully sent order_data: " . json_encode($order_data) . " \nresponse: $response , status: $status");
+                 }
+                 curl_close($ch);
 
 
 		}
